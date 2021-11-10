b9f973ddea6c4344d9120605e70b8453e9b9e89a
shevaughn98 <shevaughnlegall@gmail.com>
2021-11-10 15:41:16 +0000

    cc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//write your code here
+// first is to add a parameter to function 
+// create a for loop that checks every character of the string
+// then run a if statement 
+// then have it return a new string without capitol letters
+
+function stripUpperCase(str){
+    let string = ""
+    for(let i = 0; i < str.length; i++){
+        if(str[i] !== str[i].toUpperCase() ){
+          
+            string += str[i]
+        }
+    }
+    return string
+}
+console.log(stripUpperCase("Hello!"))
